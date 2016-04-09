5ff4a3b9a289dce45e55b6caf105f9c448040bfc
Enoch Tsang <etsang1@hotmail.com>
2016-04-09 03:17:35 -0600

    code up settings class

---
+++ InfrastructureLayer/Settings.cpp
@@ -0,0 +1,58 @@
+/**
+ *  Schulich Delta Hermes
+ *  Copyright (C) 2015 University of Calgary Solar Car Team
+ *
+ *  This file is part of Schulich Delta Hermes
+ *
+ *  Schulich Delta Hermes is free software:
+ *  you can redistribute it and/or modify it under the terms
+ *  of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Schulich Delta Hermes is distributed
+ *  in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General
+ *  Public License along with Schulich Delta Hermes.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  For further contact, email <software@calgarysolarcar.ca>
+ */
+
+#include <QSettings>
+
+#include "Settings.h"
+
+namespace
+{
+const QSettings::Format SETTINGS_FILE_FORMAT = QSettings::IniFormat;
+}
+
+Settings::Settings(QString filepath)
+: settings_(filepath, SETTINGS_FILE_FORMAT)
+{
+}
+
+const QString Settings::serialPortName() const
+{
+    return settings.value("SerialPort/portName");
+}
+
+const int Settings::baudrate() const
+{
+    return settings.value("SerialPort/baudrate");
+}
+
+const QHostAddress Settings::ipAddress() const
+{
+    return QHostAddress(settings.value("UdpAddress/ipAddress"));
+}
+
+const quint16 Settings::port() const
+{
+    return (quint16)settings.value("UdpAddress/port");
+}
---
+++ InfrastructureLayer/Settings/Settings.cpp
@@ -0,0 +1,61 @@
+/**
+ *  Schulich Delta Hermes
+ *  Copyright (C) 2015 University of Calgary Solar Car Team
+ *
+ *  This file is part of Schulich Delta Hermes
+ *
+ *  Schulich Delta Hermes is free software:
+ *  you can redistribute it and/or modify it under the terms
+ *  of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Schulich Delta Hermes is distributed
+ *  in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General
+ *  Public License along with Schulich Delta Hermes.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  For further contact, email <software@calgarysolarcar.ca>
+ */
+
+#include <QSettings>
+
+#include "Settings.h"
+
+namespace 
+{
+    const QString SETTINGS_FILE_PATH = "config.ini";
+    const QSettings::Format SETTINGS_FILE_FORMAT = QSettings::IniFormat;
+}
+
+Settings::Settings()
+: settings_(SETTINGS_FILE_PATH, SETTINGS_FILE_FORMAT)
+{
+}
+
+const QString Settings::serialPortName() const
+{
+
+}
+
+const int Settings::baudrate() const
+{
+
+}
+
+const QHostAddress Settings::ipAddress() const
+{
+
+}
+
+const quint16 Settings::port() const
+{
+
+}
+
+
