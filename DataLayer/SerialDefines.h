beb2109c5a1bbd786444c49956b79006e8844e1e
Right2Drive <bjpfannm@ucalgary.ca>
2016-10-05 22:37:04 -0400

    Changing constants to match Hermes style

--- DataLayer/SerialDefines.h
+++ DataLayer/SerialDefines.h
@@ -23,20 +23,17 @@
  *  For further contact, email <software@calgarysolarcar.ca>
  */
 
-/***************************** Solution One *************************************/
-
-
 namespace SerialDefines
 {
-    const unsigned char kKeyMotorSerialID = 1;
-    const unsigned char kMotor0DetailsSerialID = 2;
-    const unsigned char kMotor1DetailsSerialID = 3;
-    const unsigned char kDriverControlsSerialID = 4;
-    const unsigned char kMotorFaultsSerialID = 5;
-    const unsigned char kBatteryFaultsSerialID = 6;
-    const unsigned char kBatterySerialID = 7;
-    const unsigned char kCmuSerialID = 8;
-    const unsigned char kMpptSerialID = 9;
-    const unsigned char kLightsSerialID = 10;
-    const unsigned char kOtherSerialID = 11;
+    const unsigned char KEY_MOTOR_SERIAL_ID = 1;
+    const unsigned char MOTOR_0_DETAILS_SERIAL_ID = 2;
+    const unsigned char MOTOR_1_DETAILS_SERIAL_ID = 3;
+    const unsigned char DRIVER_CONTROLS_SERIAL_ID = 4;
+    const unsigned char MOTOR_FAULTS_SERIAL_ID = 5;
+    const unsigned char BATTERY_FAULTS_SERIAL_ID = 6;
+    const unsigned char BATTERY_SERIAL_ID = 7;
+    const unsigned char CMU_SERIAL_ID = 8;
+    const unsigned char MPPT_SERIAL_ID = 9;
+    const unsigned char LIGHTS_SERIAL_ID = 10;
+    const unsigned char OTHER_SERIAL_ID = 11;
 }
