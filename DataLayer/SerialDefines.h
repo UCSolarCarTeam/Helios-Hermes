eb519845cab5cced87a67c76f5b3edd5f210b803
Right2Drive <bjpfannm@ucalgary.ca>
2016-10-05 20:45:11 -0400

    Fixing SerialDefines, name convention pending

--- DataLayer/SerialDefines.h
+++ DataLayer/SerialDefines.h
@@ -25,47 +25,18 @@
 
 /***************************** Solution One *************************************/
 
- class SerialDefines
- {
-public:
-    /* Serial Gets */
-    unsigned char getKeyMotorID() const;
-    unsigned char getMotor0DetailsID() const;
-    unsigned char getMotor1DetailsID() const;
-    unsigned char getDriverControlsID() const;
-    unsigned char getMotorFaultsID() const;
-    unsigned char getBatteryFaultsID() const;
-    unsigned char getBatteryID() const;
-    unsigned char getCmuID() const;
-    unsigned char getMpptID() const;
-    unsigned char getLightsID() const;
-    unsigned char getOtherID() const;
 
-private:
-    const unsigned char keyMotorID_;
-    const unsigned char motor0DetailsID_;
-    const unsigned char motor1DetailsID_;
-    const unsigned char driverControlsID_;
-    const unsigned char motorFaultsID_;
-    const unsigned char batteryFaultsID_;
-    const unsigned char batteryID_;
-    const unsigned char cmuID_;
-    const unsigned char mpptID_;
-    const unsigned char lightsID_;
-    const unsigned char otherID_;
- }
- // With implementation in cpp file... or
-
- /***************************** Solution Two *************************************/
-
-const unsigned char kKeyMotorSerialID = 1;
-const unsigned char kMotor0DetailsSerialID = 2;
-const unsigned char kMotor1DetailsSerialID = 3;
-const unsigned char kDriverControlsSerialID = 4;
-const unsigned char kMotorFaultsSerialID = 5;
-const unsigned char kBatteryFaultsSerialID = 6;
-const unsigned char kBatterySerialID = 7;
-const unsigned char kCmuSerialID = 8;
-const unsigned char kMpptSerialID = 9;
-const unsigned char kLightsSerialID = 10;
-const unsigned char kOtherSerialID = 11;
+namespace SerialDefines
+{
+    const unsigned char kKeyMotorSerialID = 1;
+    const unsigned char kMotor0DetailsSerialID = 2;
+    const unsigned char kMotor1DetailsSerialID = 3;
+    const unsigned char kDriverControlsSerialID = 4;
+    const unsigned char kMotorFaultsSerialID = 5;
+    const unsigned char kBatteryFaultsSerialID = 6;
+    const unsigned char kBatterySerialID = 7;
+    const unsigned char kCmuSerialID = 8;
+    const unsigned char kMpptSerialID = 9;
+    const unsigned char kLightsSerialID = 10;
+    const unsigned char kOtherSerialID = 11;
+}
