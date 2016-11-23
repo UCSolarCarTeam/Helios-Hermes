8531cab10f7f513542ae24ae71362e677b208940
Right2Drive <bradpf@outlook.com>
2016-11-22 19:53:28 -0500

    Enhancing CmuUnit voltages & temps

--- CommunicationLayer/DataPopulators/CmuPopulator.cpp
+++ CommunicationLayer/DataPopulators/CmuPopulator.cpp
@@ -11,28 +11,13 @@ void CmuPopulator::populateData(const CmuMessage message)
     I_CmuUnit* cmuUnit = cmuData_.getCmuUnit(message.cmuNumber());
     QList<unsigned short> cellVoltages = message.cellVoltages();
     QList<unsigned short> cellTemperatures = message.cellTemperatures();
-    cmuUnit->setCell0Voltage(cellVoltage.pop_front());
-    cmuUnit->setCell1Voltage(cellVoltage.pop_front());
-    cmuUnit->setCell2Voltage(cellVoltage.pop_front());
-    cmuUnit->setCell3Voltage(cellVoltage.pop_front());
-    cmuUnit->setCell4Voltage(cellVoltage.pop_front());
-    cmuUnit->setCell5Voltage(cellVoltage.pop_front());
-    cmuUnit->setCell6Voltage(cellVoltage.pop_front());
-    cmuUnit->setCell7Voltage(cellVoltage.pop_front());
+    for (int i = 0; i < cellVoltages.size(); i++)
+    {
+        cmuUnit->setCellVoltage(i, cellVoltages.pop_front());
+    }
     cmuUnit->setPcbTemp(message.pcbTemperature());
-    cmuUnit->setCellTemp0(cellTemperature.pop_front());
-    cmuUnit->setCellTemp1(cellTemperature.pop_front());
-    cmuUnit->setCellTemp2(cellTemperature.pop_front());
-    cmuUnit->setCellTemp3(cellTemperature.pop_front());
-    cmuUnit->setCellTemp4(cellTemperature.pop_front());
-    cmuUnit->setCellTemp5(cellTemperature.pop_front());
-    cmuUnit->setCellTemp6(cellTemperature.pop_front());
-    cmuUnit->setCellTemp7(cellTemperature.pop_front());
-    cmuUnit->setCellTemp8(cellTemperature.pop_front());
-    cmuUnit->setCellTemp9(cellTemperature.pop_front());
-    cmuUnit->setCellTemp10(cellTemperature.pop_front());
-    cmuUnit->setCellTemp11(cellTemperature.pop_front());
-    cmuUnit->setCellTemp12(cellTemperature.pop_front());
-    cmuUnit->setCellTemp13(cellTemperature.pop_front());
-    cmuUnit->setCellTemp14(cellTemperature.pop_front());
+    for (int i = 0; i < cellTemperatures.size(); i++)
+    {
+        cmuUnit->setCellTemp(i, cellTemperatures.pop_front());
+    }
 }
--- DataLayer/CmuData/FakeCmuUnit.h
+++ DataLayer/CmuData/FakeCmuUnit.h
@@ -10,54 +10,12 @@ public:
 
     /* CmuUnit Gets */
     unsigned char getCmuNumber() const;
-    unsigned short getCell0Voltage() const;
-    unsigned short getCell1Voltage() const;
-    unsigned short getCell2Voltage() const;
-    unsigned short getCell3Voltage() const;
-    unsigned short getCell4Voltage() const;
-    unsigned short getCell5Voltage() const;
-    unsigned short getCell6Voltage() const;
-    unsigned short getCell7Voltage() const;
+    short getCellVoltage(const int&) const;
     unsigned short getPcbTemp() const;
-    unsigned short getCellTemp0() const;
-    unsigned short getCellTemp1() const;
-    unsigned short getCellTemp2() const;
-    unsigned short getCellTemp3() const;
-    unsigned short getCellTemp4() const;
-    unsigned short getCellTemp5() const;
-    unsigned short getCellTemp6() const;
-    unsigned short getCellTemp7() const;
-    unsigned short getCellTemp8() const;
-    unsigned short getCellTemp9() const;
-    unsigned short getCellTemp10() const;
-    unsigned short getCellTemp11() const;
-    unsigned short getCellTemp12() const;
-    unsigned short getCellTemp13() const;
-    unsigned short getCellTemp14() const;
+    unsigned short getCellTemp(const int&) const;
 
     /* CmuUnit Sets */
-    void setCell0Voltage(const unsigned short&);
-    void setCell1Voltage(const unsigned short&);
-    void setCell2Voltage(const unsigned short&);
-    void setCell3Voltage(const unsigned short&);
-    void setCell4Voltage(const unsigned short&);
-    void setCell5Voltage(const unsigned short&);
-    void setCell6Voltage(const unsigned short&);
-    void setCell7Voltage(const unsigned short&);
+    void setCellVoltage(const int&, const short&);
     void setPcbTemp(const unsigned short&);
-    void setCellTemp0(const unsigned short&);
-    void setCellTemp1(const unsigned short&);
-    void setCellTemp2(const unsigned short&);
-    void setCellTemp3(const unsigned short&);
-    void setCellTemp4(const unsigned short&);
-    void setCellTemp5(const unsigned short&);
-    void setCellTemp6(const unsigned short&);
-    void setCellTemp7(const unsigned short&);
-    void setCellTemp8(const unsigned short&);
-    void setCellTemp9(const unsigned short&);
-    void setCellTemp10(const unsigned short&);
-    void setCellTemp11(const unsigned short&);
-    void setCellTemp12(const unsigned short&);
-    void setCellTemp13(const unsigned short&);
-    void setCellTemp14(const unsigned short&);
+    void setCellTemp(const int&, const unsigned short&);
 }
