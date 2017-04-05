590e8f3b76f24f8cf06140a44b2814ac9dab091d
Louis Johnson <Louis Johnson>
2017-04-04 20:10:42 -0600

    modified .gitignore file, removed DeltaHermes files, and ensured branch is up-to-date

--- SchulichDeltaHermes/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "SchulichDeltaHermes.h"
-#include <QApplication>
-
-int main(int argc, char* argv[])
-{
-    QScopedPointer<SchulichDeltaHermes> app;
-    app.reset(new SchulichDeltaHermes(argc, argv));
-    return app->exec();
-}
