9799219456d2837dfc57e870328d721ea2ea41a5
Enoch Tsang <etsang1@hotmail.com>
2017-11-19 23:37:14 -0700

    remove old logger service file

--- BusinessLayer/BusinessContainer.h
+++ BusinessLayer/BusinessContainer.h
@@ -7,7 +7,6 @@ class DataContainer;
 class I_CommunicationsMonitoringService;
 class I_JsonForwarder;
 class InfrastructureContainer;
-class LoggerService;
 class I_JsonMessageBuilder;
 
 class BusinessContainer
@@ -21,7 +20,6 @@ public:
     I_CommunicationsMonitoringService& communicationsMonitoringService();
 
 private:
-    QScopedPointer<LoggerService> loggerService_;
     QScopedPointer<I_CommunicationsMonitoringService> communicationsMonitoringService_;
     QScopedPointer<I_JsonMessageBuilder> jsonMessageBuilder_;
     QScopedPointer<I_JsonForwarder> jsonForwarder_;
