77c6a04d030468c7379ce1d075c4a8b7cd6e84fd
Safa <safa.syed@ucalgary.ca>
2018-07-21 10:35:06 -0600

    removed comments

--- CommunicationLayer/DataPopulators/CcsPopulator.cpp
+++ CommunicationLayer/DataPopulators/CcsPopulator.cpp
@@ -9,20 +9,16 @@ CcsPopulator::CcsPopulator(I_PacketChecksumChecker& checksumChecker, I_CcsData& 
     connect(&checksumChecker_, SIGNAL(validDataReceived(QByteArray)), this, SLOT(validData()));
     connect(timer_, SIGNAL(timeout()), this, SLOT(timerExpired()));
     timer_->start(3000);
-    qDebug("CcsPopulator");
 }
 
 void CcsPopulator::validData()
 {
     timer_->start();
     ccsData_.setCcsAlive(true);
-    qDebug("helloooooo");
-    //qDebug() << timer_;
 }
 
 void CcsPopulator::timerExpired()
 {
     ccsData_.setCcsAlive(false);
-    qDebug("hello");
 }
 
