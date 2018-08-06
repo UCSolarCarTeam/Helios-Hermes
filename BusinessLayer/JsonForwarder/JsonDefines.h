b0c677c653cbc95b7ded21019731875f61f09f03
Bill Luu <billluu619@gmail.com>
2018-08-06 11:48:56 -0600

    Change the defines of two battery messages (#57)
    * Change the defines of two battery messages
    
    * Fix tests

--- BusinessLayer/JsonForwarder/JsonDefines.h
+++ BusinessLayer/JsonForwarder/JsonDefines.h
@@ -135,9 +135,9 @@ namespace JsonFormat
     const QString FAN_VOLTAGE = "FanVoltage";
     const QString PACK_CURRENT = "PackCurrent";
     const QString PACK_VOLTAGE = "PackVoltage";
-    const QString PACK_STATE_OF_CHARGE = "PackStateofCharge";
+    const QString PACK_STATE_OF_CHARGE = "PackStateOfCharge";
     const QString PACK_AMPHOURS = "PackAmphours";
-    const QString PACK_DEPTH_OF_DISCHARGE = "PackDepthofDischarge";
+    const QString PACK_DEPTH_OF_DISCHARGE = "PackDepthOfDischarge";
     const QString HIGH_TEMPERATURE = "HighTemperature";
     const QString HIGH_THERMISTOR_ID = "HighThermistorId";
     const QString LOW_TEMPERATURE = "LowTemperature";
