8f5b1fcbaaa13adaba0672c44bf2d1e57168b7c0
bill-luu <billluu619@gmail.com>
2018-11-03 15:45:07 -0600

    added call to developmentresoureces scripts

--- HermesInstallDependencies.sh
+++ HermesInstallDependencies.sh
@@ -11,55 +11,15 @@ fi
     libboost-chrono-dev \
     libboost-system-dev 
 
-cd ..
-mv Epsilon-Hermes ./src
-mkdir -p Epsilon-Hermes/build/.lib/
-mv src Epsilon-Hermes/
-cd Epsilon-Hermes/src
+( 
+  cd /tmp
+  git clone https://github.com/UCSolarCarTeam/Development-Resources.git
+  cd Development-Resources/InstallScripts
+  ./rabbitmq-setup.sh 
+  ./googletest-setup.sh 
+)
 
-    # install googletest and googlemock
-    (cd /tmp
-    git clone https://github.com/google/googletest.git 
-     cp -r googletest/googletest/include/gtest /usr/local/include
-     cp -r googletest/googlemock/include/gmock /usr/local/include
-    (cd googletest &&
-       g++ -std=c++11 
-           -isystem googletest/include/
-           -Igoogletest
-           -isystem googlemock/include/
-           -Igooglemock
-           -pthread
-           -c
-           googlemock/src/gmock-all.cc &&
-       g++ -std=c++11 
-           -isystem googletest/include/
-           -Igoogletest
-           -isystem googlemock/include/
-           -Igooglemock
-           -pthread
-           -c
-           googletest/src/gtest-all.cc &&
-       ar -rv libgmock.a gtest-all.o gmock-all.o ))
-    
-    cp /tmp/googletest/libgmock.a ../build/.lib/
-    # install rabbitmq-c
-    (cd /tmp
-    git clone https://github.com/alanxz/rabbitmq-c
-     cp rabbitmq-c/librabbitmq/*.h /usr/local/include/
-    mkdir rabbitmq-c/build
-    (cd rabbitmq-c/build && cmake .. && cmake --build .)
-     cp rabbitmq-c/build/librabbitmq/librabbitmq.a /usr/local/lib/
-     cp rabbitmq-c/build/librabbitmq/*.so* /usr/local/lib/)
-
-    # install SimpleAmqpClient
-    (cd /tmp
-    git clone https://github.com/alanxz/SimpleAmqpClient
-    mkdir SimpleAmqpClient/build
-    (cd SimpleAmqpClient/build &&
-       cmake -DRabbitmqc_INCLUDE_DIR=../../rabbitmq-c/librabbitmq
-             -DRabbitmqc_LIBRARY=../../rabbitmq-c/build/librabbitmq
-             .. &&
-       make)
-     mkdir /usr/local/include/SimpleAmqpClient
-     cp SimpleAmqpClient/build/*.so* /usr/local/lib/
-     cp SimpleAmqpClient/src/SimpleAmqpClient/*.h /usr/local/include/SimpleAmqpClient/)
+(
+  mkdir -p ./build/.lib 
+  cp /usr/local/lib/libgmock.a ./build/.lib
+)
