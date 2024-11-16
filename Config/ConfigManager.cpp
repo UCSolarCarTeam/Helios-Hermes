5eda8a13f72b6e1447a108561c27e566c8f5961a
Mason Bullock <mason.bullock@ucalgary.ca>
2024-11-15 22:06:40 -0700

    ConfigManager class for reading config.ini settings

--- Config/ConfigManager.cpp
+++ Config/ConfigManager.cpp
@@ -1,3 +1,90 @@
 #include "ConfigManager.h"
 
-ConfigManager::ConfigManager() {}
+#include <QSettings>
+#include <QFile>
+#include <QFileInfo>
+#include <QCoreApplication>
+#include <QDir>
+
+/** Singleton instance of ConfigManager */
+ConfigManager& ConfigManager::instance() {
+    static ConfigManager instance(QDir(QCoreApplication::applicationDirPath()).filePath("config.ini"));
+    return instance;
+}
+
+/** Read ini file and assign settings values */
+ConfigManager::ConfigManager(const QString& path){
+    qDebug() << "Reading Config File at: " << path;
+    QSettings settings(path, QSettings::IniFormat);
+
+    if (!QFile::exists(path)) {
+        qDebug() << "WARNING: Config file does not exist at path:" << path;
+        qDebug() << "Default settings will be used, this may cause unexpected behavior";
+        return;
+    }
+
+    forwardPeriod = settings.value("forwardPeriod", 500).toInt();
+
+    settings.beginGroup("UnitNumbers");
+    numberOfMotors = settings.value("numberOfMotors", 2).toInt();
+    numberOfMppts = settings.value("numberOfMppts", 4).toInt();
+    settings.endGroup();
+
+    settings.beginGroup("JsonFormat");
+    packetTitle = settings.value("packetTitle", "Hermes").toString();
+    settings.endGroup();
+
+    settings.beginGroup("SerialPort");
+    portName = settings.value("portName", "/dev/ttyUSB0").toString();
+    baudrate = settings.value("baudrate", 115200).toInt();
+    settings.endGroup();
+
+    settings.beginGroup("Mqtt");
+    localTopic = settings.value("localTopic", "hermesExchange").toString();
+    localHostname = settings.value("localHostname", "127.0.0.1").toString();
+    localPort = settings.value("localPort", 6969).toInt();
+    telemetryTopic = settings.value("telemetryTopic", "packet").toString();
+    telemetryHostname = settings.value("telemetryHostname", "aedes.solarcar.ca").toString();
+    telemetryPort = settings.value("telemetryPort", 1883).toInt();
+    telemetryUsername = settings.value("telemetryUsername", "YouDontBelongHere").toString();
+    telemetryPassword = settings.value("telemetryPassword", "QuitBeingNaughty").toString();
+    settings.endGroup();
+
+    settings.beginGroup("Logging");
+    logLevel = settings.value("logLevel", 3).toInt();
+    loggingEnabled = settings.value("loggingEnabled", true).toBool();
+    settings.endGroup();
+}
+
+//Getters
+int ConfigManager::getForwardPeriod() const { return forwardPeriod; }
+
+int ConfigManager::getNumberOfMotors() const { return numberOfMotors; }
+
+int ConfigManager::getNumberOfMppts() const { return numberOfMppts; }
+
+QString ConfigManager::getPacketTitle() const { return packetTitle; }
+
+QString ConfigManager::getPortName() const { return portName; }
+
+int ConfigManager::getBaudrate() const { return baudrate; }
+
+QString ConfigManager::getLocalTopic() const { return localTopic; }
+
+QString ConfigManager::getLocalHostname() const { return localHostname; }
+
+int ConfigManager::getLocalPort() const { return localPort; }
+
+QString ConfigManager::getTelemetryTopic() const { return telemetryTopic; }
+
+QString ConfigManager::getTelemetryHostname() const { return telemetryHostname; }
+
+int ConfigManager::getTelemetryPort() const { return telemetryPort; }
+
+QString ConfigManager::getTelemetryUsername() const { return telemetryUsername; }
+
+QString ConfigManager::getTelemetryPassword() const { return telemetryPassword; }
+
+int ConfigManager::getLogLevel() const { return logLevel; }
+
+bool ConfigManager::getLoggingEnabled() const { return loggingEnabled; }
--- Config/ConfigManager.h
+++ Config/ConfigManager.h
@@ -1,10 +1,57 @@
 #ifndef CONFIGMANAGER_H
 #define CONFIGMANAGER_H
 
-class ConfigManager
-{
+#include <QString>
+
+class ConfigManager {
+
 public:
-    ConfigManager();
+    static ConfigManager& instance();
+
+    ConfigManager(const ConfigManager&) = delete;
+    ConfigManager& operator=(const ConfigManager&) = delete;
+
+    int getForwardPeriod() const;
+
+    int getNumberOfMotors() const;
+    int getNumberOfMppts() const;
+
+    QString getPacketTitle() const;
+
+    QString getPortName() const;
+    int getBaudrate() const;
+
+    QString getLocalTopic() const;
+    QString getLocalHostname() const;
+    int getLocalPort() const;
+    QString getTelemetryTopic() const;
+    QString getTelemetryHostname() const;
+    int getTelemetryPort() const;
+    QString getTelemetryUsername() const;
+    QString getTelemetryPassword() const;
+
+    int getLogLevel() const;
+    bool getLoggingEnabled() const;
+
+private:
+    ConfigManager(const QString& path);
+
+    int forwardPeriod;
+    int numberOfMotors;
+    int numberOfMppts;
+    QString packetTitle;
+    QString portName;
+    int baudrate;
+    QString localTopic;
+    QString localHostname;
+    int localPort;
+    QString telemetryTopic;
+    QString telemetryHostname;
+    int telemetryPort;
+    QString telemetryUsername;
+    QString telemetryPassword;
+    int logLevel;
+    bool loggingEnabled;
 };
 
 #endif // CONFIGMANAGER_H
