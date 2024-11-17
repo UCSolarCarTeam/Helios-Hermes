65149a79ed55a3bdbad0a759082d2d456690a9cc
Mason Bullock <mason.bullock@ucalgary.ca>
2024-11-17 13:27:25 -0700

    added function to interface for extracting data from byteStream

--- PacketFactory/Packets/IPacket.h
+++ PacketFactory/Packets/IPacket.h
@@ -5,10 +5,37 @@
 #include <QByteArray>
 
 class IPacket {
-    public:
-        virtual ~IPacket() {}
-        virtual void populatePacket(const QByteArray& data) = 0;
-        virtual QJsonObject toJson() = 0;
+public:
+    virtual ~IPacket() {}
+    virtual void populatePacket(const QByteArray& data) = 0;
+    virtual QJsonObject toJson() = 0;
+
+    /** Extracts value of type T from QByteArray at startIndex */
+    template <class T>
+    T getValue(const QByteArray& data, int startIndex) {
+        if(startIndex < 0 || startIndex + sizeof(T) > data.size()) {
+            qWarning() << "Invalid start index for packet data";
+            return T(0);
+        }
+
+        QDataStream stream(data.mid(startIndex, sizeof(T)));
+        stream.setByteOrder(QDataStream::LittleEndian);
+
+        if constexpr (std::is_floating_point_v<T>) {
+            stream.setFloatingPointPrecision(QDataStream::SinglePrecision);
+        }
+
+        return readStream<T>(stream);
+    }
+
+private:
+    /** Reads value of type T from QDataStream */
+    template <class T>
+    T readStream(QDataStream& stream) {
+        T value;
+        stream >> value;
+        return value;
+    }
 };
 
 #endif // IPACKET_H
