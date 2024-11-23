5c345c12aeb4567382c898b73cebbd54d4eebe93
SolarCar <adeebhossain2004@gmail.com>
2024-11-23 13:50:40 -0700

    got the messaging stuff working, need GPIOReader to work still

--- PacketFactory/Packets/PiPacket.cpp
+++ PacketFactory/Packets/PiPacket.cpp
@@ -1,10 +1,22 @@
 #include "PiPacket.h"
+#include "../../Config/JsonDefinitions.h"
 #include <QDebug>
 
+PiPacket::PiPacket() {
+    wiegandData_ = 0;
+}
 
 void PiPacket::populatePacket(const QByteArray& data) {
     setData(data);  // Use the setter to store Wiegand data
-    qDebug() << "Populated packet with Wiegand data:" << data;
+    // qDebug() << "Populated packet with Wiegand data:" << data;
+}
+
+QJsonObject PiPacket::toJson() {
+    QJsonObject json;
+
+    json[JsonDefinitions::WIEGAND_DATA] = getDataAsLongLong();
+
+    return json;
 }
 
 QByteArray PiPacket::getData() const {
